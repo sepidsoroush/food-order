c3aee36a14602a0892ca4bc1ab7cbf4f5de920dd
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-05-14 11:19:24 +0300

    add AvailableMeals component to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import MealsSummary from "./components/MealsSummary";
+import AvailableMeals from "./components/AvailableMeals";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MealsSummary />
+      <AvailableMeals />
     </div>
   );
 }
---
+++ src/components/AvailableMeals.js
@@ -0,0 +1,12 @@
+import styles from "../styles/AvailableMeals.module.css";
+import DUMMY_MEALS from "../data/dummy-meals";
+
+const Header = () => {
+  return (
+    <div className={styles.meals}>
+      <ul>test</ul>
+    </div>
+  );
+};
+
+export default Header;
