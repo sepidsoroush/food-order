b19636743aa16a532cb4ef2a54e8cbcbc2aafbf4
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-05-14 11:12:48 +0300

    add MealsSummary component to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import MealsSummary from "./components/MealsSummary";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MealsSummary />
     </div>
   );
 }
