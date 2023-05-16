46d2099be19a59ae72c9e24b2425224ef481affe
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-05-16 10:38:52 +0300

    add totalOrders number to HeaderCartButton

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,20 @@
+import { useContext } from "react";
 import styles from "../../styles/HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../context/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const totalOrders = cartCtx.items.reducer((currentNum, item) => {
+    return currentNum + item.amount;
+  }, 0);
   return (
     <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>0</span>
+      <span className={styles.badge}>{totalOrders}</span>
     </button>
   );
 };
