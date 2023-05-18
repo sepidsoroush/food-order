8adefaff9e4a65c567391b9a90d9995462cba4fd
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-05-18 16:41:17 +0300

    fetch data from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,26 +1,75 @@
 import styles from "../../styles/AvailableMeals.module.css";
-import DUMMY_MEALS from "../../assets/dummy-meals";
 import MealItem from "./MealItem";
 import Card from "../UI/Card";
+import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [httpError, setHttpError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        "https://food-order-5faa4-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+
+      let loadedData = [];
+      for (const key in data) {
+        loadedData.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(loadedData);
+      setIsLoading(false);
+    };
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles.loading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={styles.error}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
   return (
-    <Card className={styles.meals}>
-      <ul>
-        {DUMMY_MEALS.map((meal) => {
-          const { id, name, description, price } = meal;
-          return (
-            <MealItem
-              key={id}
-              id={id}
-              name={name}
-              price={price}
-              description={description}
-            />
-          );
-        })}
-      </ul>
-    </Card>
+    <div className={styles.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </div>
   );
 };
 
